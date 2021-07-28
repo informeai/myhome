7f57bc9cb375a8840937636d22c19731e4030d9f
Wellington Gadelha <contato.informeai@gmail.com>
2021-07-28 17:58:37 -0300

    add banner effect and img of banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Banner.css'
+import banner1 from '../assets/banner1.jpg'
 
 const Banner = () =>{
 	return (
-		<div id="banner"></div>
+		<div id="banner">
+		<img id="image" src={banner1}/>
+		</div>
 	)
 }
 
