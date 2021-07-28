ee9193ed22166dbaf0fd73ea826304415cc55b73
Wellington Gadelha <contato.informeai@gmail.com>
2021-07-28 12:19:21 -0300

    add effect of title card focus

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{MouseEvent} from 'react'
 import './Card.css'
 
 interface CardProps{
@@ -7,10 +7,21 @@ interface CardProps{
 }
 
 const Card = ({name, img}: CardProps): JSX.Element =>{
+		const  moveEnter = (e:MouseEvent<HTMLElement>)=>{ 
+			let target = e.target as HTMLElement
+			let span = target.nextSibling as HTMLElement
+			span!.style.transform = "translateY(25px)"
+		}
+
+		const  moveLeave = (e:MouseEvent<HTMLElement>)=>{ 
+			let target = e.target as HTMLElement
+			let span = target.nextSibling as HTMLElement
+			span!.style.transform = "translateY(0px)"
+		}
 		
 	return(
 		<div className="card">
-		<img src={img}/>
+		<img src={img} onMouseEnter={moveEnter} onMouseLeave={moveLeave}/>
 		<span>{name}</span>
 		</div>
 	)
