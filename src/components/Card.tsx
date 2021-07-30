f683efac127ad382337ab9083035360ec0189f80
Wellington Gadelha <contato.informeai@gmail.com>
2021-07-30 09:23:41 -0300

    add banner by hover in cards.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,11 @@
 import React,{MouseEvent} from 'react'
 import './Card.css'
+import bannerGeral from '../assets/banner-geral.png'
+import bannerMusic from '../assets/banner-music.png'
+import bannerMovie from '../assets/banner-movie.png'
+import bannerPhoto from '../assets/banner-photo.png'
+import bannerSetting from '../assets/banner-setting.png'
+
 
 interface CardProps{
 	name: string
@@ -7,10 +13,23 @@ interface CardProps{
 }
 
 const Card = ({name, img}: CardProps): JSX.Element =>{
+		
 		const  moveEnter = (e:MouseEvent<HTMLElement>)=>{ 
+		let imageBanner = document.querySelector("#banner img#image") as HTMLImageElement;
 			let target = e.target as HTMLElement
 			let span = target.nextSibling as HTMLElement
 			span!.style.transform = "translateY(25px)"
+			if(name === "music"){
+				imageBanner.src = bannerMusic		
+			}else if(name === "movie"){
+				imageBanner.src = bannerMovie
+			}else if(name === "photo"){
+				imageBanner.src = bannerPhoto
+			}else if(name === "setting"){
+				imageBanner.src = bannerSetting
+			}else{
+				imageBanner.src = bannerGeral
+			}
 		}
 
 		const  moveLeave = (e:MouseEvent<HTMLElement>)=>{ 
